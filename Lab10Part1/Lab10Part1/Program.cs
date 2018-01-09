ceb4f7a0731a99ed2266a237f66d00bcb68d2521
triod315 <triod315@gmail.com>
2018-01-09 21:40:03 +0200

    Lab 10 Part 1

---
+++ Lab10Part1/Lab10Part1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab10Part1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
