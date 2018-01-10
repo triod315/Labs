fe38ec69d42e040532cc69fd5e90e0552d60be6a
triod315 <triod315@gmail.com>
2018-01-10 22:02:13 +0200

    last working version

--- Lab10Part1/Lab10Part1/Program.cs
+++ Lab10Part1/Lab10Part1/Program.cs
@@ -11,57 +11,125 @@ namespace Lab10Part1
     {
         static string GetFileName()//get file name from console and check correct format
         {
-            string fname = Console.ReadLine();
+            bool err_flag = false;
+            string fname;
+            do
+            {
+                Console.WriteLine("Input file name");
+                fname = Console.ReadLine();
+                for (int i = 0; i < fname.Length; i++)
+                {
+                    if (Path.GetInvalidFileNameChars().Contains(fname[i]))
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("Error: file name contain illegal character: " + fname[i]);
+                        Console.ResetColor();
+                        err_flag = true;
+                        continue;
+                    }
+                    err_flag = false;
+                }
+            } while (err_flag);
             return fname;
         }
         static void CreateFile()//create and fill binary file
         {
-            int n;//count of elements
-            string file_name = GetFileName();
-            BinaryWriter binary_writer = new BinaryWriter(File.Open(file_name, FileMode.Create));
+            const string path = "C:\\Users\\user\\Desktop\\";
+            bool err_flag = false;
             do
-            {
-                Console.WriteLine("Write count of elements");
-            }
-            while (!int.TryParse(Console.ReadLine(), out n));//chek for correct format of n
-            int temp_var;
-            for (int i = 0; i < n; i++)
-            {
-                do
+                try
                 {
-                    Console.WriteLine("Введіть {0} елемент", i+1);
+                    int n;//count of elements
+                    string file_name =path+ GetFileName();
+                    BinaryWriter binary_writer = new BinaryWriter(File.Open(file_name, FileMode.Create));
+                    try
+                    {
+                        do
+                        {
+                            Console.WriteLine("Write count of elements");
+                        }
+                        while (!int.TryParse(Console.ReadLine(), out n) && n>1);//chek for correct format of n
+                        int temp_var;
+                        for (int i = 0; i < n; i++)
+                        {
+                            do
+                            {
+                                Console.WriteLine("Введіть {0} елемент", i + 1);
+                            }
+                            while (!int.TryParse(Console.ReadLine(), out temp_var));//chek for correct format of temp_var
+                            binary_writer.Write(temp_var);
+                        }
+                        binary_writer.Close();
+                    }
+                    catch (Exception er) { Console.WriteLine(er.Message); }
+                    finally
+                    {
+                        binary_writer.Close();
+                    }
                 }
-                while (!int.TryParse(Console.ReadLine(), out temp_var));//chek for correct format of temp_var
-                binary_writer.Write(temp_var);
-            }
-            binary_writer.Close();
+                catch (Exception e)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Error: " + e.Message);
+                    Console.ResetColor();
+                    err_flag = true;
+                }
+            while (err_flag);
         }
 
         static void Main(string[] args)
         {
-            CreateFile();
-            int sum = 0;
-            int max_sum = -65535;
-            int temp_elem;
-            int prevois_elem=-65535;
-            string file_name = GetFileName();
-            BinaryReader bianry_reader = new BinaryReader(File.Open(file_name, FileMode.Open));
-            
-            while (bianry_reader.BaseStream.Position != bianry_reader.BaseStream.Length)
+            try
             {
-                temp_elem = bianry_reader.ReadInt32();
-                if (temp_elem > prevois_elem) sum += temp_elem;
-                else
+                const string path = "C:\\Users\\user\\Desktop\\";
+                CreateFile();
+                int counter = 0;
+                int sum = 0;
+                int max_sum = -65535;
+                int temp_elem;
+                int prevois_elem = -65535;
+                string file_name;
+                do
                 {
-                    if (sum > max_sum) max_sum = sum;
-                    sum = 0;
-                    sum += temp_elem;
+                    file_name =path+ GetFileName();
+                } while (!File.Exists(file_name));
+
+
+                BinaryReader bianry_reader = new BinaryReader(File.Open(file_name, FileMode.Open));
+                try
+                {
+                    while (bianry_reader.BaseStream.Position != bianry_reader.BaseStream.Length)
+                    {
+                        temp_elem = bianry_reader.ReadInt32();
+                        counter++;
+                        if (temp_elem <= prevois_elem) { sum = 0; counter = 0; }
+                        sum += temp_elem;
+                        prevois_elem = temp_elem;
+                        if (sum > max_sum && counter > 1) max_sum = sum;
+                    }
+                    if (max_sum == -65535)
+                        Console.WriteLine("Growing sequence not found");
+                    else
+                        Console.WriteLine("Max sum=" + max_sum);
+                    bianry_reader.Close();
                 }
-                prevois_elem = temp_elem;
-                if(bianry_reader.BaseStream.Position == bianry_reader.BaseStream.Length) if (sum > max_sum) max_sum = sum;
+                catch (Exception e) { Console.WriteLine(e.Message); }
+                finally { bianry_reader.Close(); }
             }
-            Console.WriteLine("Max sum="+max_sum);
-            bianry_reader.Close();
+            catch (FormatException fe)
+            {
+                Console.WriteLine(fe.Message);
+            }
+            catch (OverflowException oe)
+            {
+                Console.WriteLine(oe.Message);
+            }
+            catch (Exception err)
+            {
+                Console.BackgroundColor = ConsoleColor.Red;
+                Console.WriteLine("FATAL ERROR: "+err.Message);
+                Console.ResetColor();
+            }
         }
     }
 }
