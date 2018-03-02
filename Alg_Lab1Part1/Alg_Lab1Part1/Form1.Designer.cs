9453b43895a31c6d0e152474c6aa5120ed1d19f8
triod315 <triod315@gmail.com>
2018-03-02 11:25:50 +0200

    input count of points

--- Alg_Lab1Part1/Alg_Lab1Part1/Form1.Designer.cs
+++ Alg_Lab1Part1/Alg_Lab1Part1/Form1.Designer.cs
@@ -35,26 +35,32 @@
             this.button1 = new System.Windows.Forms.Button();
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
             this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.inputToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.saveAsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripSeparator();
             this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.inputToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.inputTableToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.chart2 = new System.Windows.Forms.DataVisualization.Charting.Chart();
             this.chart3 = new System.Windows.Forms.DataVisualization.Charting.Chart();
             this.chart4 = new System.Windows.Forms.DataVisualization.Charting.Chart();
             this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
             this.menuStrip1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.chart2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.chart3)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.chart4)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(145, 1);
+            this.button1.Location = new System.Drawing.Point(1143, 349);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 1;
@@ -86,41 +92,50 @@
             this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
             this.fileToolStripMenuItem.Text = "&File";
             // 
+            // saveToolStripMenuItem
+            // 
+            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(112, 22);
+            this.saveToolStripMenuItem.Text = "&Save";
+            // 
+            // saveAsToolStripMenuItem
+            // 
+            this.saveAsToolStripMenuItem.Name = "saveAsToolStripMenuItem";
+            this.saveAsToolStripMenuItem.Size = new System.Drawing.Size(112, 22);
+            this.saveAsToolStripMenuItem.Text = "&Save as";
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(109, 6);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(112, 22);
+            this.exitToolStripMenuItem.Text = "&Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
             // inputToolStripMenuItem
             // 
+            this.inputToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.inputTableToolStripMenuItem});
             this.inputToolStripMenuItem.Name = "inputToolStripMenuItem";
             this.inputToolStripMenuItem.Size = new System.Drawing.Size(47, 20);
             this.inputToolStripMenuItem.Text = "&Input";
+            // 
+            // inputTableToolStripMenuItem
+            // 
+            this.inputTableToolStripMenuItem.Name = "inputTableToolStripMenuItem";
+            this.inputTableToolStripMenuItem.Size = new System.Drawing.Size(131, 22);
+            this.inputTableToolStripMenuItem.Text = "&Input table";
+            this.inputTableToolStripMenuItem.Click += new System.EventHandler(this.inputTableToolStripMenuItem_Click);
             // 
             // helpToolStripMenuItem
             // 
             this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
             this.helpToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
             this.helpToolStripMenuItem.Text = "&Help";
-            // 
-            // saveToolStripMenuItem
-            // 
-            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
-            this.saveToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
-            this.saveToolStripMenuItem.Text = "&Save";
-            // 
-            // saveAsToolStripMenuItem
-            // 
-            this.saveAsToolStripMenuItem.Name = "saveAsToolStripMenuItem";
-            this.saveAsToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
-            this.saveAsToolStripMenuItem.Text = "&Save as";
-            // 
-            // toolStripMenuItem1
-            // 
-            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
-            this.toolStripMenuItem1.Size = new System.Drawing.Size(149, 6);
-            // 
-            // exitToolStripMenuItem
-            // 
-            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
-            this.exitToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
-            this.exitToolStripMenuItem.Text = "&Exit";
-            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
             // 
             // chart2
             // 
@@ -162,16 +177,58 @@
             this.chart1.TabIndex = 6;
             this.chart1.Text = "chart1";
             // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.comboBox1);
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(1074, 40);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(200, 300);
+            this.panel1.TabIndex = 7;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Linear interpolation",
+            "Lagarnge intrpolation"});
+            this.comboBox1.Location = new System.Drawing.Point(18, 57);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(165, 26);
+            this.comboBox1.TabIndex = 2;
+            this.comboBox1.Text = "Interpolation type";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox1.Location = new System.Drawing.Point(37, 15);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(146, 26);
+            this.textBox1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(15, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(16, 18);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "h";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1284, 661);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.panel1);
             this.Controls.Add(this.chart1);
             this.Controls.Add(this.chart4);
             this.Controls.Add(this.chart3);
             this.Controls.Add(this.chart2);
-            this.Controls.Add(this.button1);
             this.Controls.Add(this.menuStrip1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.MainMenuStrip = this.menuStrip1;
@@ -186,6 +243,8 @@
             ((System.ComponentModel.ISupportInitialize)(this.chart3)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.chart4)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -205,6 +264,11 @@
         private System.Windows.Forms.DataVisualization.Charting.Chart chart3;
         private System.Windows.Forms.DataVisualization.Charting.Chart chart4;
         private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ToolStripMenuItem inputTableToolStripMenuItem;
     }
 }
 
--- Alg_Lab1Part1/Alg_Lab1Part1/Form1.cs
+++ Alg_Lab1Part1/Alg_Lab1Part1/Form1.cs
@@ -18,9 +18,16 @@ namespace Alg_Lab1Part1
             InitializeComponent();
         }
 
+        /// <summary>
+        /// Заповнює масив x з заданим кроком
+        /// </summary>
+        /// <param name="from">початок проміжку</param>
+        /// <param name="h">крок</param>
+        /// <param name="to">кінець проміжку</param>
+        /// <returns>масив x</returns>
         static double[] GenereteXarray(double from, double h, double to)
         {
-            int n = (int)((to - from) / h) + 2;
+            int n = (int)((to - from) / h) + 1;
             double[] x_arr = new double[n];
             x_arr[0] = from;
             for (int i = 1; i < n; i++)
@@ -30,8 +37,97 @@ namespace Alg_Lab1Part1
             return x_arr;
         }
 
+
+        /// <summary>
+        /// Лінійна апроксимація функції, заданої тчками
+        /// </summary>
+        /// <param name="in_x_arr">масив x координат вузлів</param>
+        /// <param name="in_y_arr">масив у координат вузлів</param>
+        /// <param name="out_x_arr"> масив х аргументів функції</param>
+        /// <param name="out_y_arr">вихідний масив значень функції</param>
+        static void LinearInterpolation(double[] in_x_arr, double[] in_y_arr, double[] out_x_arr, double[] out_y_arr)
+        {
+            double[] a = new double[in_x_arr.Length - 1];
+            double[] b = new double[in_x_arr.Length - 1];
+
+            for (int i = 0; i < in_x_arr.Length - 1; i++)
+            {
+                a[i] = (in_y_arr[i + 1] - in_y_arr[i]) / (in_x_arr[i + 1] - in_x_arr[i]);
+                b[i] = in_y_arr[i] - a[i] * in_x_arr[i];
+            }
+
+            int j = 0;
+            for (int i = 0; i < out_x_arr.Length; i++)
+            {
+                for (j = 0; j < in_x_arr.Length - 1; j++)
+                {
+                    if (out_x_arr[i] < in_x_arr[j + 1] && out_x_arr[i] > in_x_arr[j]) break;
+                }
+
+                out_y_arr[i] = a[j] * out_x_arr[i] + b[j];
+            }
+        }
+
+        /// <summary>
+        /// апроксимація функції методом Лагранжа
+        /// </summary>
+        /// <param name="in_x_arr">масив x координат вузлів</param>
+        /// <param name="in_y_arr">масив у координат вузлів</param>
+        /// <param name="out_x_arr">вихідний масив аргументів функції</param>
+        /// <param name="out_y_arr">вихідний масив значень фукції</param>
+        static void LagrangeInterpolation(double[] in_x_arr, double[] in_y_arr, double[] out_x_arr, double[] out_y_arr)
+        {
+            double l = 1;
+            for (int j = 0; j < out_x_arr.Length; j++)
+            {
+                out_y_arr[j] = 0;
+                for (int i = 0; i < in_y_arr.Length; i++)
+                {
+                    for (int k = 0; k < in_y_arr.Length; k++)
+                    {
+                        if (k != i)
+                            l *= (out_x_arr[j] - in_x_arr[k]) / (in_x_arr[i] - in_x_arr[k]);
+                    }
+
+                    out_y_arr[j] += in_y_arr[i] * l;
+                    l = 1;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Обчислює значення першої татдругої поїхідної шляхом числового диференціювання
+        /// </summary>
+        /// <param name="out_x_arr">масив аргументів функції</param>
+        /// <param name="in_y_arr">масив значень функції</param>
+        /// <param name="first_diff_y">масив значень першої похідної</param>
+        /// <param name="secon_diff_y">масив значень другої похідної</param>
+        /// <param name="h">крок</param>
+        static void Differentiate(double[] out_x_arr, double[] in_y_arr, double[] first_diff_y, double[] secon_diff_y, double h)
+        {
+            //для обислення похідної в крайніх точках використано формули правої та лівої похідної
+            first_diff_y[0] = (in_y_arr[1] - in_y_arr[0]) / h;
+            first_diff_y[out_x_arr.Length - 1] = (in_y_arr[out_x_arr.Length - 1] - in_y_arr[out_x_arr.Length - 2]) / h;
+
+            for (int i = 1; i < out_x_arr.Length - 1; i++)
+            {
+                first_diff_y[i] = (in_y_arr[i + 1] - in_y_arr[i - 1]) / (2 * h);
+            }
+
+            //для обчислення другої похідної в крайніх точках використано формули правої та лівої похідної та значення першої похідної
+            secon_diff_y[0] = (first_diff_y[1] - first_diff_y[0]) / h;
+            secon_diff_y[out_x_arr.Length - 1] = (first_diff_y[out_x_arr.Length - 1] - first_diff_y[out_x_arr.Length - 2]) / h;
+
+            for (int i = 1; i < out_x_arr.Length - 1; i++)
+            {
+                secon_diff_y[i] = (in_y_arr[i + 1] + in_y_arr[i - 1] - 2 * in_y_arr[i]) / (h * h);
+            }
+        }
+
+
         static void ShowChart(Chart chart, double[] in_x_arr, double[] in_y_arr)
         {
+            chart.Series.Clear();
             Series ser = new Series();
             ser.ChartType = SeriesChartType.Point;
             for (int i = 0; i < in_x_arr.Length; i++)
@@ -47,7 +143,8 @@ namespace Alg_Lab1Part1
             System.Threading.Thread.CurrentThread.CurrentCulture = new System.Globalization.CultureInfo("en-US");
             double from = -1.5; //початкова межа
             double to = 1.5;    //кінцева межа
-            double h = 0.1;     //крок x в таблиці
+            double h;     //крок x в таблиці
+            do { } while (!double.TryParse(textBox1.Text, out h));
             int n = 9;          //кількість вузлів
 
             double[] x_array = { -1.6, -1.1, -0.6, -0.2, 0.2, 0.7, 0.93, 1.2, 1.51 };
@@ -66,11 +163,79 @@ namespace Alg_Lab1Part1
             ShowChart(chart1,x_array,y_array);
 
 
+
+            double[] out_y_arr = new double[out_x_array.Length];//масив значень функції отриманих апроксимацією
+
+            if (comboBox1.SelectedIndex == 0)
+            {
+                LinearInterpolation(x_array, y_array, out_x_array, out_y_arr);
+            }
+            else
+            {
+                LagrangeInterpolation(x_array, y_array, out_x_array, out_y_arr);
+            }
+
+            ShowChart(chart2, out_x_array, out_y_arr);
+
+            double[] first_diff_y = new double[out_x_array.Length];//масив значень похідної функції отриманих числовим дифференціюванням
+            double[] second_diff_y = new double[out_x_array.Length];//масив значень другої похідної функції отриманих числовим дифференціюванням
+
+            Differentiate(out_x_array, out_y_arr, first_diff_y, second_diff_y, h);
+            ShowChart(chart3,out_x_array,first_diff_y);
+            ShowChart(chart4, out_x_array, second_diff_y);
+
         }
+
+        private void GetPoints()
+        {
+            int n; 
+
+            Form n_input = new Form();
+            Label inf_txt = new Label();
+            TextBox n_string = new TextBox();
+
+ 
+            n_input.Height = 150;
+            inf_txt.Text = "Input count of points";
+            inf_txt.Width = 120;
+            inf_txt.TextAlign = ContentAlignment.MiddleCenter;
+            inf_txt.Location = new Point(n_input.Width/2-inf_txt.Width/2,10);
+            n_string.Location = new Point(n_input.Width / 2 - n_string.Width / 2, inf_txt.Height+10);
+            n_input.FormBorderStyle = FormBorderStyle.FixedDialog;
+            n_input.Controls.Add(inf_txt);
+            n_input.Controls.Add(n_string);
+            n_input.MinimizeBox = false;
+            n_input.MaximizeBox = false;
+            Button btn = new Button();
+            btn.Location = new Point(n_input.Width / 2 - btn.Width / 2, inf_txt.Height+n_string.Height + 15);
+            btn.Text = "OK";
+            void Ok_clik(object sender, EventArgs e)
+            {
+                try
+                {
+                    n = int.Parse(n_string.Text);
+                    n_input.Close();
+                } catch (Exception) { }
+            }
+            btn.Click += new EventHandler(Ok_clik);
+            n_input.AcceptButton = btn;
+            n_input.Controls.Add(btn);
+            n_input.StartPosition = FormStartPosition.CenterScreen;
+
+            n_input.ShowDialog();
+
+        }
+
+        
 
         private void exitToolStripMenuItem_Click(object sender, EventArgs e)
         {
             Application.Exit();
+        }
+
+        private void inputTableToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            GetPoints();
         }
     }
 }
