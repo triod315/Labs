e0d03f7255148295fc804fffa5ba086f97487ecf
triod315 <triod315@gmail.com>
2018-04-03 18:07:16 +0300

    better version of Lab12

--- Lab12/Lab12/Program.cs
+++ Lab12/Lab12/Program.cs
@@ -1,9 +1,13 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-namespace Lab12
+namespace Lab12_Console
 {
-    enum Answer {a,b,c,d};
+
+    enum Answer { a, b, c, d };
     class Question
     {
         /// <summary>
@@ -33,8 +37,8 @@ namespace Lab12
             get
             {
                 return mark;
-            }   
-            set 
+            }
+            set
             {
                 if (value > 0)
                     mark = value;
@@ -43,39 +47,18 @@ namespace Lab12
             }
         }
 
+        public override string ToString() => $"Question {text} \n({mark} points)";
 
-        /// <summary>
-        /// default constructor
-        /// Initializes a new instance of the <see cref="Lab12.Question"/> class.
-        /// </summary>
-        public Question()
-        {
-            text="Question";
-            mark = 0;
-        }
 
-        /// <summary>
-        /// Initializes a new instance of the <see cref="Lab12.Question"/> class.
-        /// </summary>
-        /// <param name="q_text">Question text</param>
-        /// <param name="q_mark">Q mark.</param>
-        /// <param name="q_type">Q type.</param>
-        public Question(string q_text,double q_mark)
-        {
-            text = q_text;
-            mark = q_mark;
-        }
     }
 
-    class SingleChoiseQuestion:Question
+    class SingleChoiseQuestion : Question
     {
         private Answer correct_answer;
         public Answer Correct_answer
         {
-            get
-            {
-                return correct_answer;
-            }
+            get { return correct_answer; }
+            
             set
             {
                 correct_answer = value;
@@ -87,39 +70,36 @@ namespace Lab12
         /// </summary>
         /// <returns>mark</returns>
         /// <param name="answer">Answer.</param>
-        /// <param name="question">Question.</param>
-        public static double CheckAnswer(Answer answer, SingleChoiseQuestion question)
+        public double CheckAnswer(Answer answer)
         {
-            if (answer == question.Correct_answer)
-                return question.Mark;
-            else
-                return 0;
+            if (answer == Correct_answer)
+                return Mark;
+            return 0;
         }
-
+                
         /// <summary>
         /// Default consturctor
         /// </summary>
-        public SingleChoiseQuestion():this("Question 1",Answer.a,0){ }
+        public SingleChoiseQuestion() : this("Question 1", Answer.a, 0) { }
 
         /// <summary>
         /// Initializes a new instance of the <see cref="Lab12.SingleChoiseQuestion"/> class.
         /// </summary>
         /// <param name="correct_answ">Correct answer.</param>
-        public SingleChoiseQuestion(string q_text,Answer correct_answ, double q_mark)
+        public SingleChoiseQuestion(string q_text, Answer correct_answ, double q_mark)
         {
             Text = q_text;
             Mark = q_mark;
             Correct_answer = correct_answ;
-        }
-
+        }          
     }
 
-    class ShortAnswerQuestion:Question
+    class ShortAnswerQuestion : Question
     {
         private string correct_answer;
         public string Correct_answer
         {
-            get 
+            get
             {
                 return correct_answer;
             }
@@ -128,10 +108,7 @@ namespace Lab12
                 if (value.Length >= 0)
                     correct_answer = value;
                 else
-                {
-                    Exception Incorrect_short_answer_exception = new Exception("Incorrect answer");
-                    throw Incorrect_short_answer_exception;
-                }
+                    throw new Exception("Incorrect answer format");
             }
         }
 
@@ -141,18 +118,17 @@ namespace Lab12
         /// <returns>mark</returns>
         /// <param name="answer">Answer.</param>
         /// <param name="question">Question.</param>
-        public static double CheckAnswer(String answer, ShortAnswerQuestion question)
+        public double CheckAnswer(String answer)
         {
-            if (answer == question.Correct_answer)
-                return question.Mark;
-            else
-                return 0;
+            if (answer == Correct_answer)
+                return Mark;
+            return 0;
         }
 
         /// <summary>
         /// Default constructor
         /// </summary>
-        public ShortAnswerQuestion():this("Question","empty filed",0){}
+        public ShortAnswerQuestion() : this("Question", "empty filed", 0) { }
 
         /// <summary>
         /// Initializes a new instance of the <see cref="Lab12.ShortAnswerQuestion"/> class.
@@ -160,7 +136,7 @@ namespace Lab12
         /// <param name="q_text">text of questin.</param>
         /// <param name="correct_answ">Correct answer.</param>
         /// <param name="q_mark">Q mark.</param>
-        public ShortAnswerQuestion(string q_text,string correct_answ, double q_mark)
+        public ShortAnswerQuestion(string q_text, string correct_answ, double q_mark)
         {
             Text = q_text;
             Correct_answer = correct_answ;
@@ -170,103 +146,97 @@ namespace Lab12
 
     }
 
+    class QuestionsCollection<T>
+    {
+        private T[] arr;
+        private int length;
+        /// <summary>
+        /// Constructor of colection
+        /// </summary>
+        /// <param name="size">count of elements in collection</param>
+        public QuestionsCollection(int size)
+        {
+            arr = new T[size];
+            length = size;
+        }
+
+
+        /// <summary>
+        /// Length of collection
+        /// </summary>
+        public int Length
+        {
+            get { return length; }
+        }
+
+        /// <summary>
+        /// indexator
+        /// </summary>
+        /// <param name="i">index</param>
+        /// <returns>QuestionCollection element</returns>
+        public T this[int i]
+        {
+            get
+            {
+                if (i >= 0 && i < length)
+                    return arr[i];
+                else
+                    throw new Exception("Question not found (index out of range)");
+            }
+            set
+            {
+                if (i >= 0 && i < length && value != null)
+                    arr[i] = value;
+                else
+                    throw new Exception("Wrong format of question");
+            }
+        }
+    }
+
     class Test
     {
-        private List<SingleChoiseQuestion> single_choise_questions;
+        private QuestionsCollection<SingleChoiseQuestion> single_choise_questions;
 
-        public List<SingleChoiseQuestion> Single_choise_questions
+        public QuestionsCollection<SingleChoiseQuestion> Single_choise_questions
         {
-            get 
+            set
+            {
+                if (value != null)
+                    single_choise_questions = value;
+                else
+                    throw new Exception("Wrong format of QuestionsCollections");
+            }
+            get
             {
                 return single_choise_questions;
             }
         }
 
-        private List<ShortAnswerQuestion> short_answer_questions;
+        private QuestionsCollection<ShortAnswerQuestion> short_answer_questions;
 
-        public List<ShortAnswerQuestion> Short_answer_questions
+        public QuestionsCollection<ShortAnswerQuestion> Short_answer_questions
         {
+            set
+            {
+                if (value != null)
+                    short_answer_questions = value;
+                else
+                    throw new Exception("Wrong format of QuestionsCollections");
+            }
             get
             {
                 return short_answer_questions;
             }
         }
 
-        /// <summary>
-        /// Adds the single choise question.
-        /// </summary>
-        /// <param name="question">Question.</param>
-        public void Add_Question(SingleChoiseQuestion question)
-        {
-            if (question != null)
-            {
-                single_choise_questions.Add(question);
-                max_mark += question.Mark;
-            }
-            else
-            {
-                Exception Wrong_scq_exc = new Exception("Wrong single choise format");
-                throw Wrong_scq_exc;
-            }
-        }
-
-        /// <summary>
-        /// Adds the short answer question.
-        /// </summary>
-        /// <param name="question">Question.</param>
-        public void Add_Question(ShortAnswerQuestion question)
-        {
-            if (question != null)
-            {
-                short_answer_questions.Add(question);
-                max_mark += question.Mark;
-            }
-            else
-            {
-                Exception Wrong_saq_exc = new Exception("Wrong short answer format");
-                throw Wrong_saq_exc;
-            }
-                
-        }
-
-        /// <summary>
-        /// Checks the test.
-        /// </summary>
-        /// <returns>real mark</returns>
-        /// <param name="test">Test.</param>
-        /// <param name="short_answers">Short answers.</param>
-        /// <param name="scq_answers">Single answer question answers.</param>
-        public static double CheckTest(Test test,string[] short_answers, Answer[] scq_answers)
-        {
-            double mark = 0;
-
-            for (int i=0;i<short_answers.GetLength(0);i++)
-                mark+=ShortAnswerQuestion.CheckAnswer(short_answers[i],test.short_answer_questions[i]);
-                    
-            for (int i = 0; i < scq_answers.Length; i++)
-            {
-                mark += SingleChoiseQuestion.CheckAnswer(scq_answers[i], test.single_choise_questions[i]);
-            }
-            return mark;
-        }
-
-        public static double CheckTest(Test test,string[] short_answers)
-        {
-            double mark = 0;
-
-            for (int i=0;i<short_answers.GetLength(0);i++)
-                mark+=ShortAnswerQuestion.CheckAnswer(short_answers[i],test.short_answer_questions[i]);
-            return mark;
-        }
-
         private string name;
         public string Name
         {
-            get 
+            get
             {
                 return name;
             }
-            set 
+            set
             {
                 if (value.Length != 0)
                     name = value;
@@ -278,83 +248,196 @@ namespace Lab12
         private double max_mark;
         public double Max_Mark
         {
-            get 
+            get
             {
-                max_mark = 0;
-                for (int i = 0; i < single_choise_questions.Count; i++)
-                    max_mark += single_choise_questions[i].Mark;
-                for (int i = 0; i < short_answer_questions.Count; i++)
-                    max_mark += short_answer_questions[i].Mark;
-                return max_mark;
+               return max_mark;
             }
-        }
 
+        }
         /// <summary>
         /// Default constructor
         /// </summary>
 
-        public Test():this("Unnamed test"){}
+        public Test() : this("Unnamed test",0,0) { }
 
-        public Test(string test_name)
+        public Test(string test_name, int saq_count, int siq_count)
         {
-            name=test_name;
+            name = test_name;
             max_mark = 0;
-            single_choise_questions = new List<SingleChoiseQuestion>();
-            short_answer_questions = new List<ShortAnswerQuestion>();
+            single_choise_questions = new QuestionsCollection<SingleChoiseQuestion>(siq_count);
+            short_answer_questions = new QuestionsCollection<ShortAnswerQuestion>(saq_count);
         }
 
         public static Test operator +(Test test1, Test test2)
         {
-            Test test = new Test();
-            test.name = test1.name +" "+ test2.name;
+            Test test = new Test(test1.name + test2.name, test1.short_answer_questions.Length + test2.short_answer_questions.Length, test1.single_choise_questions.Length + test2.single_choise_questions.Length);
+            test.name = test1.name + " " + test2.name;
 
-            test.single_choise_questions = test1.single_choise_questions;
-            for (int i = 0; i < test2.single_choise_questions.Count; i++)
+            for (int i = 0; i < test1.single_choise_questions.Length; i++)
             {
-                test.single_choise_questions.Add(test2.single_choise_questions[i]);
+                test.single_choise_questions[i] = test1.single_choise_questions[i];
             }
 
-            test.short_answer_questions = test1.short_answer_questions;
-            for (int i = 0; i < test2.short_answer_questions.Count; i++)
+            for (int i = 0; i < test2.single_choise_questions.Length; i++)
             {
-                test.short_answer_questions.Add(test2.short_answer_questions[i]);
+                test.single_choise_questions[i+test1.single_choise_questions.Length] = test2.single_choise_questions[i];
+            }
+
+            for (int i = 0; i < test1.short_answer_questions.Length; i++)
+            {
+                test.short_answer_questions[i] = test1.short_answer_questions[i];
+            }
+
+            for (int i = 0; i < test2.short_answer_questions.Length; i++)
+            {
+                test.short_answer_questions[i+test1.short_answer_questions.Length]=test2.short_answer_questions[i];
             }
 
             test.max_mark = test1.max_mark + test2.max_mark;
             return test;
         }
 
-        public override string ToString() => $"{this.name}, max mark:{this.Max_Mark}";
+        public override string ToString() => $"{this.name}";
 
     }
 
-
-
-    class MainClass
+    class StudentsTest
     {
+        private string students_name;
+        public string Students_name
+        {
+            get { return students_name; }
+            set
+            {
+                if (value.Length != 0) students_name = value;
+            }
+        }
+        private Test s_test;
 
-       
+        public Test S_test
+        {
+            get { return s_test; }
+        }
 
+        private double mark;
+        public double Mark
+        {
+            get
+            {
+                mark = 0;
+                for (int i = 0; i < siq_answers.Length; i++)
+                {
+                    mark+=s_test.Single_choise_questions[i].CheckAnswer(siq_answers[i]);
+                }
+
+                for (int i = 0; i < short_answers.Length; i++)
+                {
+                    mark+=s_test.Short_answer_questions[i].CheckAnswer(short_answers[i]);
+                }
+                return mark;
+            }
+        }
+
+        private Answer[] siq_answers;
+        public Answer[] Siq_answes
+        {
+            get { return siq_answers; }
+            set
+            {
+                siq_answers=value;
+            }
+        }
+
+        private string[] short_answers;
+        public string[] Short_answers
+        {
+            get { return short_answers; }
+            set
+            {
+                short_answers = value;
+            }
+        }
+
+        public StudentsTest(string s_name,Test test)
+        {
+            students_name = s_name;
+            s_test = test;
+            siq_answers = new Answer[test.Single_choise_questions.Length];
+            short_answers = new string[test.Short_answer_questions.Length];
+        }
+
+        public override string ToString() => $"\n\nResult\nStudents name:{students_name} \nTest: {s_test.Name}\nResult: {Mark}\n\n";
+    }
+
+
+class Program
+    {
+        static Test CreateTest(string test_name, string[] q_texts, string[] correct_saq_answer, Answer[] correct)
+        {
+            Test test = new Test(test_name,correct_saq_answer.Length,correct.Length);
+            Console.WriteLine("Test'"+test.ToString() + "' created successfully" );
+            for (int i = 0; i < correct_saq_answer.GetLength(0); i++)
+            {
+                test.Short_answer_questions[i] = new ShortAnswerQuestion(q_texts[i], correct_saq_answer[i], 10);
+            }
+            for (int i = 0; i < correct.Length; i++)
+            {
+                test.Single_choise_questions[i] = new SingleChoiseQuestion(q_texts[i+correct_saq_answer.GetLength(0)], correct[i], 5);
+            }
+            return test;
+        }
+
+
+        static StudentsTest DoTest(Test test, string user_name)
+        {
+            Console.WriteLine("----------------------------Exam begin-------------------------------------");
+            Console.WriteLine(test.ToString());
+
+            StudentsTest s_test = new StudentsTest(user_name,test);
+
+            Answer[] sicansw = new Answer[test.Single_choise_questions.Length];//single choise user answer
+
+            for (int i = 0; i < test.Single_choise_questions.Length; i++)
+            {
+                Console.WriteLine(test.Single_choise_questions[i].ToString());
+                s_test.Siq_answes[i] = test.Single_choise_questions[i].Correct_answer;
+            }
+
+            string[] saq_answers = new string[test.Short_answer_questions.Length];
+
+            for (int i = 0; i < test.Short_answer_questions.Length; i++)
+            {
+                Console.WriteLine(test.Short_answer_questions[i].ToString());
+                s_test.Short_answers[i] = test.Short_answer_questions[i].Correct_answer;
+            }
+
+            Console.WriteLine("-------------------------------Exam end------------------------------------\n");
+
+            Console.WriteLine(s_test.ToString());
+            return s_test;
+        }      
         public static void Main(string[] args)
         {
-            Test students_pain = new Test("Programing exam");
-            students_pain.Add_Question(new ShortAnswerQuestion("Are you ready for exam?", "Yes", 5));
-            students_pain.Add_Question(new ShortAnswerQuestion("Pain 2", "o no o no o no no no", 10));
-            Console.WriteLine(students_pain.Short_answer_questions[0].Text+" "+students_pain.Short_answer_questions[0].Correct_answer);
-
-            string[] answer = { "babaika", "o no o no o no no "};
-            double mark = Test.CheckTest(students_pain, answer);
-            if (mark != students_pain.Max_Mark)
-                Console.WriteLine("You are in the army now");
             
+                string[] tasks1 = { "Are you ready for exam? ", "Are you ready to cry?", "Secelct your weapoon" };
+                string[] test1_correct_answ = { "Yes", "No" };
+                Answer[] test1 = { Answer.a };
+                Test students_pain = CreateTest("Programing", tasks1, test1_correct_answ, test1);
 
-            Test highwaytohell = new Test("Statistics exam");
-            highwaytohell.Add_Question(new ShortAnswerQuestion("Are you alive?","Yes",5));
+                StudentsTest s_test1 = DoTest(students_pain, "Sacrifice");
 
-            Test fatality = highwaytohell + students_pain;
-            //Console.WriteLine(fatality.Max_Mark);
-            Console.WriteLine(fatality.ToString());
+                string[] tasks2 = { "8/4", "1/2" };
+                string[] test2_correct_answ = { "2", "0.5" };
+                Answer[] test2 = new Answer[0];
 
+                Test highwaytohell = CreateTest("Statistics", tasks2, test2_correct_answ,test2);
+                StudentsTest s_test2 = DoTest(highwaytohell, "Sacrafice");
+
+                Test fatality = highwaytohell + students_pain;
+
+                StudentsTest s_test3 = DoTest(fatality, "Sacrafice");
+                
+            
         }
     }
 }
