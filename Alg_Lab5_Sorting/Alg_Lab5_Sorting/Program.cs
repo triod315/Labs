3165cc06c029edd4bf44d1b7207b66ded4f339a4
triod315 <triod315@gmail.com>
2018-04-14 22:52:50 +0300

    full version

--- Alg_Lab5_Sorting/Alg_Lab5_Sorting/Program.cs
+++ Alg_Lab5_Sorting/Alg_Lab5_Sorting/Program.cs
@@ -1,9 +1,10 @@
-﻿using System;
+using System;
 
 namespace Alg_Lab5_Sorting
 {
     class MainClass
     {
+
         /// <summary>
         /// Swap the specified a and b.
         /// </summary>
@@ -44,6 +45,7 @@ namespace Alg_Lab5_Sorting
                 if (i < j)
                     Swap<double>(ref a[i],ref a[j]);
             }
+            PrintArray(a);
             QuickSort(a, p, j);
             QuickSort(a,j+1,q);
         }
@@ -86,8 +88,17 @@ namespace Alg_Lab5_Sorting
             {
                 for (int i = d; i < a.Length; i++)
                     for (int j = i; j >= d && a[j - d] > a[j]; j -= d)
+                    {
                         Swap<double>(ref a[j], ref a[j - d]);
+                        PrintArray(a);
+                    }
             }
+        }
+
+        public static void ChangeArr(double[] arr)
+        {
+            for (int i = 0; i < arr.Length / 2; i++)
+                Swap<double>(ref arr[i],ref arr[arr.Length - i-1]);
         }
 
         /// <summary>
@@ -105,16 +116,29 @@ namespace Alg_Lab5_Sorting
             }
             return c;
         }
+            
 
         public static void Main (string[] args)
         {
+            
+
             Console.WriteLine("A");
             double[] a = GetArrayFromConsole();
             Console.WriteLine("B");
             double[] b = GetArrayFromConsole();
             double[] c = CreateNewArray(a, b);
+            bool is_uprising = ((a.Length > 1 && a[a.Length - 1] > a[0]) || (b.Length > 1 && b[b.Length - 1] > b[0]));
+
+            Console.WriteLine("Quick ");
             QuickSort (c,0,c.Length-1);
-            //ShellSort(a);
+
+            Console.WriteLine("Shell sort");
+            ShellSort(c);
+            PrintArray(c);
+            if (is_uprising)
+                ChangeArr(c);
+            
+
             PrintArray(c);
 
 
