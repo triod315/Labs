50c400ca3bad084a397cec37536d5604da01f093
triod315 <triod315@gmail.com>
2018-04-25 00:16:42 +0300

    BFS update, DFS(depth first search) added

--- Alg_Lab7/main.cpp
+++ Alg_Lab7/main.cpp
@@ -29,28 +29,22 @@ vector<int> BFS(vector<vector<double> > &matrix, int start_pos, int end_pos)
     q.push(start_pos);
     vector<bool> used(matrix.size());//vector of used points
     vector<int> p(matrix.size());
-    vector<double> dist(matrix.size());
 
     used[start_pos]=true;
     p[start_pos]=-1;
-
-	copy(used.begin(),used.end(),ostream_iterator<bool>(cout," "));
-
+	
     int v;
-    int to;
     while (!q.empty())
     {
         v=q.front();
         q.pop();
         for (int i=0;i<matrix[v].size();i++)
         {
-            to=i;
-            if (!used[to] && matrix[v][i]==1)
+            if (!used[i] && matrix[v][i]==1)
             {
-                used[to]=true;
-                q.push(to);
-                dist[to]=dist[v]+;
-                p[to]=v;
+                used[i]=true;
+                q.push(i);
+                p[i]=v;
             }
         }
     }
@@ -61,30 +55,58 @@ vector<int> BFS(vector<vector<double> > &matrix, int start_pos, int end_pos)
         path.push_back(v);
     reverse(path.begin(),path.end());
     return path;
-
-
 }
 
+vector<int> DFS(vector<vector<double> > &matrix, int start_pos, int end_pos)
+{
+	stack<int> s;
+	vector<int> p(matrix.size());
+	vector<bool> used(matrix.size());
+	s.push(start_pos);
+	int v;
+	used[start_pos]=true;
+	p[start_pos]=-1;
+	while (!s.empty())
+		{
+			v=s.top();
+			s.pop();
+			for (int i=0;i<matrix.size();i++)
+				{
+					if (!used[i] && matrix[v][i]==1)
+					{
+                        s.push(i);
+						used[v]=true;
+						p[i]=v;
+					}
+				}
+		}
+		
+		
+    vector<int> path;
+    for (int v=end_pos;v!=-1;v=p[v])
+        path.push_back(v);
+    reverse(path.begin(),path.end());
+	return path;
+}
 
 int main()
 {
-    vector <vector <double> > graph=GetMatrics(10);
-   /* {
-        {0,1,1,1,0,0,0,0,0,0},
-        {1,0,0,0,1,0,0,0,0,0},
-        {1,0,0,0,0,1,1,0,0,0},
-        {1,0,0,0,0,0,0,1,0,0},
-        {0,1,0,0,0,0,0,0,1,0},
-        {0,0,1,0,0,0,0,0,0,1},
-        {0,0,1,0,0,0,0,0,0,0},
-        {0,0,0,1,0,0,0,0,0,0},
-        {0,0,0,0,1,0,0,0,0,0},
-        {0,0,0,0,0,1,0,0,0,0}
-    };*/
-    int n;//count of vertices(points)
+	int n;//count of vertices(points)
+	cout<<"input count vertices\n";
+	cin>>n;
+	cout<<"Write incedense matrix\n";
+    vector <vector <double> > graph=GetMatrics(n);//incedense matrix
+
+	int start_point,end_point;
+	
+	cout<<"enter start point: ";
+	cin>>start_point;
+	cout<<"\nenter end point: ";
+	cin>>end_point;
+
     PrintMatrix(graph);
 
-    vector<int> way=BFS(graph,0,9);
+    vector<int> way=BFS(graph,start_point,end_point);
     copy(way.begin(),way.end(),ostream_iterator<int>(cout," "));
 
     return 0;
